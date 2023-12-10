0970510350ab7b7e96dfe73e83ed9f57721743f0
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-09 16:08:00 -0800

    Update Prize-draw.js

--- Prize-draw.js
+++ Prize-draw.js
@@ -34,3 +34,16 @@ Notes:
 
 
 // Solution
+
+function rank(st, we, n) {
+	let names = st.split(',')
+  if (!st.length) return 'No participants'
+  if (names.length < n) return 'Not enough participants'
+  return names.map((_, i) => ({
+    	name: _,
+      s: [..._.toLowerCase()].reduce((a, b) => a + b.charCodeAt() - 95, 0) * we[i]
+    }))
+    .sort((a, b) => a.name > b.name)
+    .sort((a, b) => b.s - a.s)
+    [n - 1].name
+}
